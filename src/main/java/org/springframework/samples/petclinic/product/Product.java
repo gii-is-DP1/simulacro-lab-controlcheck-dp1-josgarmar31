8bdaba151a3925a01d55877bdb56d2cf1d071a9c
Jose Luis Garcia Marin <josgarmar31@alum.us.es>
2021-12-21 12:25:03 +0100

    Control check

--- src/main/java/org/springframework/samples/petclinic/product/Product.java
+++ src/main/java/org/springframework/samples/petclinic/product/Product.java
@@ -1,13 +1,28 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+
+import org.springframework.samples.petclinic.model.NamedEntity;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Product {
-    Integer id;
-    String name;
+@Entity
+@Table(name = "products")
+public class Product extends NamedEntity{
+    
+   
+   @Min(0)
     double price;
+
+    @ManyToOne
+    @JoinColumn(name = "producttype")
     ProductType productType;
 }
